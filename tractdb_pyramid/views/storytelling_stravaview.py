af68915878cd5d05530c59b467238b188ba18cd6
Daniel Epstein <daniel.a.epstein@gmail.com>
2017-05-26 14:02:37 -0700

    Add an access_token endpoint to Strava

--- tractdb_pyramid/views/storytelling_stravaview.py
+++ tractdb_pyramid/views/storytelling_stravaview.py
@@ -16,6 +16,14 @@ service_runs = cornice.Service(
     cors_credentials=True
 )
 
+service_access_token = cornice.Service(
+    name='storytelling_strava_access_token',
+    path='/storytelling/strava/access_token/{code}',
+    description='Set the access token by exchanging the code provided by Strava',
+    cors_origins=('*',),
+    cors_credentials=True
+)
+
 
 def _get_admin(request):
     # Create our admin object
@@ -26,6 +34,63 @@ def _get_admin(request):
     )
 
     return admin
+
+@service_access_token.post()
+def set_access_token(request):
+    """ Exchange the provided code for an access token and store it
+    """
+
+    # Our strava code
+    code = request.matchdict['code']
+
+    # If we don't have a strava secret, we can't make the key exchange
+    if 'strava' not in request.registry.settings['secrets']:
+        request.response.status_int = 500
+        return
+
+    # Exchange the code for an access token
+    # The API will throw an unspecified error if code is invalid.
+    # Catch that rather than taking down the server.
+    access_token = ""
+
+    try:
+        client = StravaClient()
+        access_token = client.exchange_code_for_token(client_id=request.registry.settings['secrets']['strava']['strava_id'],
+            client_secret=request.registry.settings['secrets']['strava']['strava_secret'],
+            code=code)
+    except:
+        # Return appropriately
+        request.response.status_int = 500
+        return
+
+    # Our admin object
+    admin = _get_admin(request)
+
+    #Create the access token if it doesn't exist, or update the stored one
+    if not admin.exists_document('strava_access_token'):
+        # Store the access token
+        admin.create_document({'strava_access_token': access_token}, doc_id='strava_access_token')
+    else:
+        # Update the stored access token
+        try:
+            admin.update_document({'strava_access_token': access_token, '_id':'strava_access_token'})
+        except:
+            # By design, "update" seems to throw an exception? Not really sure why.
+            # But return correctly if it worked.
+            request.response.status_int = 200
+            return {
+                'access_token':
+                    access_token
+            }
+        
+
+    # Return appropriately
+    request.response.status_int = 200
+    return {
+        'access_token':
+            access_token
+    }
+    
 
 
 @service_runs.get()
@@ -65,7 +130,7 @@ def get_runs(request):
             }
             if a.map.summary_polyline is not None:
                 run['map_polyline'] = a.map.summary_polyline
-            if a.start_latlng is not None and a.start_date is not None:
+            if a.start_latlng is not None and a.start_date is not None and 'darksky' in request.registry.settings['secrets']:
                 fio = ForecastIO.ForecastIO(
                     request.registry.settings['secrets']['darksky']['darksky_secret'],
                     units=ForecastIO.ForecastIO.UNITS_US,
